57fae84d195b4b505803405237a115680b71ce42
pcbibinf <pcbibinf@BIBINF-N09.dma.ulpgc.es>
2017-12-07 11:10:33 +0000

    Primera versión kata6

--- src/kata6/main/Kata6.java
+++ src/kata6/main/Kata6.java
@@ -1,17 +1,17 @@
-package kata4.main;
+package kata6.main;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.List;
-import kata4.model.Histogram;
-import kata4.view.HistogramDisplay;
-import kata4.view.MailHistogramBuilder;
-import kata4.view.MailListBuilder;
+import kata6.model.Histogram;
+import kata6.view.HistogramDisplay;
+import kata6.view.MailHistogramBuilder;
+import kata6.view.MailListBuilder;
 
-public class Kata4 {
+public class Kata6 {
 
     public static void main(String[] args) throws IOException {
-        Kata4 kata4 = new Kata4();
+        Kata6 kata4 = new Kata6();
         kata4.execute();
     }
     
@@ -26,7 +26,7 @@ public class Kata4 {
     }
      
      private void input() throws FileNotFoundException, IOException{
-        String filename = "C:\\Users\\usuario\\Documents\\Kata3-master\\Kata3-master\\src\\kata4\\emails.txt";
+        String filename = "C:\\Users\\pcbibinf\\Documents\\NetBeansProjects\\Kata4\\src\\emails.txt";
         mailList = MailListBuilder.read(filename);
     }
      
